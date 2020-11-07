f9f72ba95b64de090be2a98402d9e2d91d76be99
Br0mm <Andrew-Ost@mail.ru>
2020-11-07 15:39:48 +0300

    переделал hasUnpaintedCheck в поиск в ширину, а так же незакрашенные поля ищутся теперь от позиции робота в данный момент

--- src/main/java/DataObjects/Map.java
+++ src/main/java/DataObjects/Map.java
@@ -1,8 +1,6 @@
 package DataObjects;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
 public class Map {
     public Map(String data){
@@ -10,10 +8,12 @@ public class Map {
         generateMap();
     }
 
-    private String mapStringData;
+    private final String mapStringData;
     public Field[][] map;
     public int robotStartX;
     public int robotStartY;
+    public int currentRobotX;
+    public int currentRobotY;
     public int sizeX = 0;
     public int sizeY = 0;
     private List<Field> unpaintedFields = new ArrayList<>();
@@ -28,6 +28,8 @@ public class Map {
         direction = WallDirection.NONE;
         createObstacles(partOfInput[2]);
         createBoosters(partOfInput[3]);
+        currentRobotX = robotStartX;
+        currentRobotY = robotStartY;
     }
 
     //нужно для построения стен
@@ -183,7 +185,8 @@ public class Map {
     }
 
     public List<Field> getUnpaintedFields() {
-        hasUnpaintedCheck(map[robotStartX][robotStartY]);
+        unpaintedFields = new ArrayList<>();
+        hasUnpaintedCheck(map[currentRobotX][currentRobotY]);
         for (int k = 0; k < sizeX; k++) {
             for (int j = 0; j < sizeY; j++) {
                 map[k][j].setIsPaintedCheck(false);
@@ -193,16 +196,35 @@ public class Map {
     }
 
     private void hasUnpaintedCheck(Field start) {
-        if (!start.getIsPaintedCheck()) {
-            if (!start.getIsObstacle() && !start.getIsPainted()) unpaintedFields.add(start);
-            start.setIsPaintedCheck(true);
-            if (!start.getIsObstacle()) {
-                hasUnpaintedCheck(map[start.getX() + 1][start.getY()]);
-                if (start.getX() != 0) hasUnpaintedCheck(map[start.getX() - 1][start.getY()]);
-                hasUnpaintedCheck(map[start.getX()][start.getY() + 1]);
-                if (start.getY() != 0) hasUnpaintedCheck(map[start.getX()][start.getY() - 1]);
+        Queue<Field> fieldsToVisit = new ArrayDeque<>();
+        fieldsToVisit.add(start);
+        Field currentPosition;
+        while (!fieldsToVisit.isEmpty()) {
+            currentPosition = fieldsToVisit.remove();
+            if (!currentPosition.getIsPaintedCheck()) {
+                if (!currentPosition.getIsObstacle() && !currentPosition.getIsPainted()) unpaintedFields.add(currentPosition);
+                currentPosition.setIsPaintedCheck(true);
+                if (!currentPosition.getIsObstacle()) {
+                    fieldsToVisit.add(map[currentPosition.getX() + 1][currentPosition.getY()]);
+                    if (currentPosition.getX() != 0) fieldsToVisit.add(map[currentPosition.getX() - 1][currentPosition.getY()]);
+                    fieldsToVisit.add(map[currentPosition.getX()][currentPosition.getY() + 1]);
+                    if (currentPosition.getY() != 0) fieldsToVisit.add(map[currentPosition.getX()][currentPosition.getY() - 1]);
+                }
             }
         }
+    }
+
+    public void setCurrentRobotX(int x) {
+        currentRobotX = x;
+    }
+
+    public void setCurrentRobotY(int y) {
+        currentRobotY = y;
+    }
+
+    public void setCurrentRobotXY(int x, int y) {
+        currentRobotX = x;
+        currentRobotY = y;
     }
 
     public List<Field> getClowns(){
