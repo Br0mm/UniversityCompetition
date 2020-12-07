1f329d31564c3551fdbd4c040bdaf4f82c067e4f
Br0mm <Andrew-Ost@mail.ru>
2020-12-07 15:15:42 +0300

    фиксы

--- src/main/java/ResultGenerator/ResultGenerator.java
+++ src/main/java/ResultGenerator/ResultGenerator.java
@@ -68,6 +68,8 @@ public class ResultGenerator {
                 separators--;
             }
         }
+        commands = new ArrayList<>();
+        commands.add(new StringBuilder());
         return result.toString();
     }
 }
